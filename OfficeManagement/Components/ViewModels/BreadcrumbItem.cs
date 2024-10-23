0f5bbf378cb1467d17de15f4c01ff1c608d94a72
Sajjad Hossain <sajjad.nazmul@enosisbd.com>
2024-10-23 18:48:05 +0600

    breadcrumb implemented, dynamic NavTitle added, stream rendering implemented

---
+++ OfficeManagement/Components/ViewModels/BreadcrumbItem.cs
@@ -0,0 +1,16 @@
+﻿namespace OfficeManagement.Components.ViewModels
+{
+    public class BreadcrumbItem
+    {
+        public string Text { get; set; }
+        public string Link { get; set; }
+        public bool IsActive { get; set; }
+
+        public BreadcrumbItem(string text, string link, bool isActive = false)
+        {
+            Text = text;
+            Link = link;
+            IsActive = isActive;
+        }
+    }
+}
